5a4a014d1e4ccad8448acf67c514033e6fbb5990
kidusshoa <captainkevin2008@gmail.com>
2024-10-02 17:12:51 +0300

    geez

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Habeshan-Fidel
+
 geez fidel typing software using electron
+
+npm init -y
+
+npm install electron --save-dev
+
+npm install react react-dom
+npm install webpack webpack-cli webpack-dev-server --save-dev
+npm install @babel/core babel-loader @babel/preset-react --save-dev
+
+#starting app
+
+npm start
+npm run build
+npm run electron to build
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", // Path to your HTML file
+      filename: "index.html", // Output file in dist folder
+    }),
+  ],
+};
